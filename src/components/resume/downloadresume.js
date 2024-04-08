844db59d8322f23b1e8d03165fc132e2d0281174
Alec Martens <am400316@ohio.edu>
2024-04-08 17:28:48 -0400

    Removed extra comments

--- src/components/resume/downloadresume.js
+++ src/components/resume/downloadresume.js
@@ -4,8 +4,7 @@ import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const DownloadResume = () => {
   const handleDownload = () => {
-    // Replace 'path_to_your_resume.pdf' with the actual path to your resume PDF file
-    const resumeUrl = "path_to_your_resume.pdf";
+    const resumeUrl = ".pdf";
     const link = document.createElement("a");
     link.href = resumeUrl;
     link.download = "resume.pdf";
--- src/components/spacer.js
+++ src/components/spacer.js
@@ -20,31 +20,19 @@ const Spacer = ({
   const spacerStyle = {
     height: height || "20px",
     width: width || "100%",
-    // maxWidth: "2800px",
     backgroundColor: isDarkMode ? "#2a3a57" : "#ADD8E6", // Dark blue for dark mode, light blue for light mode
     margin: "0 auto",
-    position: "relative", // Set position to relative
-    // display: "flex",
-    // alignItems: "center",
-    // justifyContent: "space-between",
-    // padding: "0px",
-    // paddingLeft: "20px",
-    // paddingRight: "20px",
+    position: "relative",
   };
 
   const buttonContainerStyle = {
-    // display: "flex",
-    // alignItems: "center",
-    // justifyContent: "center", // Center the content horizontally
-    // height: "100%", // Ensure the container takes full height
-    position: "absolute", // Set position to absolute
-    top: "50%", // Center vertically
-    transform: "translateY(-50%)", // Adjust vertical position
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
   };
 
   return (
     <div style={spacerStyle}>
-      {/* <div style={buttonContainerStyle}> */}
       <div style={{ ...buttonContainerStyle, left: "20px" }}>
         {showHomeButton && (
           <Link
@@ -59,18 +47,14 @@ const Spacer = ({
           </Link>
         )}
       </div>
-      {/* <div style={buttonContainerStyle}> */}
-      {/* <div style={{ justifyContent: "center" }}> */}
-        {showDarkModeButton && (
-          <div style={{ buttonContainerStyle, justifyContent: "center" }}>
-            <DarkModeToggle
-              isDarkMode={isDarkMode}
-              setIsDarkMode={setIsDarkMode}
-            />
-          </div>
-        )}
-      {/* </div> */}
-      {/* <div style={buttonContainerStyle}> */}
+      {showDarkModeButton && (
+        <div style={{ buttonContainerStyle, justifyContent: "center" }}>
+          <DarkModeToggle
+            isDarkMode={isDarkMode}
+            setIsDarkMode={setIsDarkMode}
+          />
+        </div>
+      )}
       <div style={{ ...buttonContainerStyle, right: "20px" }}>
         {showDownloadButton && <DownloadResume />}
         {showResumeButton && (
