efef755e07be9606d6129e9a1069d1efd3a559e7
Alec Martens <am400316@ohio.edu>
2024-05-08 19:02:47 -0400

    Added logo.css and hide image on mobile only

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logoImage from '../assets/char1.png'; 
+import '../styles/logo.css'
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img src={logoImage} alt="Alec Martens" className="logo-img" />
+      <img src={logoImage} alt="Alec Martens" class="mobile-hide" className="logo-img" />
     </div>
   );
 }
